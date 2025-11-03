84731747aa066d9afc95ef43b23ba6016e916bdb
Níz Peña <nizadiel5@gmail.com>
2025-11-03 15:21:43 -0700

    SBU-04: Dashboard, Header y Sidebar estructurados y estilados

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,15 @@
-import Layout from '../components/common/Layout';
+import Layout from "../components/common/Layout";
+import Cards from "../components/home/Cards";
 
 function Home() {
   return (
     <Layout>
-      <div className='index-content'>
-        aqui inicia dashboard
+      <div className='home-content'>
+        <h1>Dashboard</h1>
+
+        <Cards/>
+
+        
       </div>
     </Layout>
   );
